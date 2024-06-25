0a9e8807623f0a38346fb65fbe678d7a3e64ca82
PredatorXxRIPxX <wassimou009@gmail.com>
2024-06-25 22:18:39 +0200

    arduino Project

--- src/main.cpp
+++ src/main.cpp
@@ -23,21 +23,17 @@ void setAlarm2(DateTime now) {
 }
 
 void setup() {
-  // Start serial communication
   Serial.begin(9600);
   Serial.println("Hello World");
   
-  // Initialize the LED pin as an output
   pinMode(13, OUTPUT);
   digitalWrite(13, LOW);
 
-  // Initialize the RTC
   if (!rtc.begin()) {
     Serial.println("Couldn't find RTC");
     while (1); 
   }
 
-  // Check if the RTC lost power
   if (rtc.lostPower()) {
     Serial.println("RTC lost power, let's set the time!");
     rtc.adjust(DateTime(F(__DATE__), F(__TIME__)));
@@ -48,13 +44,10 @@ void setup() {
 void loop() {
   DateTime now = rtc.now();
 
-  // Print the current seconds
   Serial.println(now.second());
 
-  // Check alarms
   setAlarm1(now);
   setAlarm2(now);
 
-  // Wait for 1 second before checking again
   delay(1000);
 }
