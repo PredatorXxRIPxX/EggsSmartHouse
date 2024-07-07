dae7844c0a0de2ea4a3119449c9c9ef4bce35c9e
PredatorXxRIPxX <wassimou009@gmail.com>
2024-07-07 15:40:03 +0200

    onistep Project

--- src/main.cpp
+++ src/main.cpp
@@ -3,14 +3,16 @@
 #include <Wire.h>
 #include <SPI.h>
 #include <Servo.h>
+#include <SoftwareSerial.h>
+#include <avr/wdt.h>
 
-
+SoftwareSerial mySerial(2, 3); 
 Servo myservo;
 RTC_DS3231 rtc;
 
-
-const int alarminute1 = 2; 
-const int alarminute2 = 3;
+String incommingData = "";
+volatile int alarminute1 = 2; 
+volatile int alarminute2 = 3;
 DateTime startedMinutes;
 volatile int degrees[] = {0,90,180};
 volatile int index = 0;
@@ -30,7 +32,6 @@ void turnServo() {
 
 void setAlarm1(long passedTime) {
   if(passedTime % (alarminute1*60) == 0){
-    digitalWrite(13, HIGH);
     Serial.println("Alarm 1");
     turnServo();
     delay(1000);
@@ -41,16 +42,48 @@ void setAlarm1(long passedTime) {
 
 void setAlarm2(long passedTime) {
   if(passedTime % (alarminute2*60) == 0){
-    digitalWrite(13, LOW);
     Serial.println("Alarm 2");
   }
 }
 
+
+void ConnectionBluetooth(){
+  if(mySerial.available()>0){
+    incommingData = mySerial.readString();
+    int data = incommingData.toInt();
+    if(data <= 27){
+      analogWrite(11,255);
+      analogWrite(10,255);
+      analogWrite(6,255);
+    }else if(data > 27 && data < 35){
+      long tmp = map(data, 38, 0, 0, 255);
+      analogWrite(11,tmp);
+      analogWrite(10,tmp);
+      analogWrite(6,0);
+      Serial.println("used tmp :"+String(tmp));
+    }else{
+      long tmp = map(data, 38, 0, 0, 255);
+      analogWrite(11,tmp);
+      analogWrite(10,0);
+      Serial.println("used tmp :"+String(tmp));
+      analogWrite(6,0);
+    }
+
+  }
+}
+
 void setup() {
+  mySerial.begin(9600);
   Serial.begin(9600);
   myservo.attach(9);
   pinMode(13, OUTPUT);
+  pinMode(6,OUTPUT);
+  pinMode(11,OUTPUT);
+  pinMode(10,OUTPUT);
   digitalWrite(13, LOW);
+  analogWrite(6,255);
+  analogWrite(11,255);
+  analogWrite(10,255);
 
   if (!rtc.begin()) {
     Serial.println("Couldn't find RTC");
@@ -67,11 +100,9 @@ void setup() {
 }
 
 void loop() {
+  ConnectionBluetooth();
   DateTime now = rtc.now();
   long elapse = (now - startedMinutes).totalseconds();
-  Serial.println(elapse);
-  
   setAlarm1(elapse);
   setAlarm2(elapse);
-  
 }
